3b200b4ea10fa21f561a2fd9465d0cce7bbfe43e
Xinjie Shen <frinkleko@gmail.com>
2024-10-16 22:21:04 +0800

    fix moon and desert may only apper one

--- src/convert_from_wjx.py
+++ src/convert_from_wjx.py
@@ -36,6 +36,7 @@ moon_dict = {
     'Solar-powered FM receiver-transmitter': '15'
 }
 
+
 def process_col_name(col_name):
     index1 = col_name.index('(')
     name = col_name[index1+1:-1]
@@ -44,44 +45,58 @@ def process_col_name(col_name):
 def process(data, task):
     new_cols = []
     for col in data.columns:
-        if col.startswith('2、') or col.startswith('7、'):
+        if col.startswith('4、') or col.startswith('7、'):
             col_name = process_col_name(col)
         else: 
             col_name = col
-        
         new_cols.append(col_name)
 
     data.columns = new_cols
 
-    desert_df = data[desert_dict.keys()]
-    moon_df = data[moon_dict.keys()]
+    no_desert = False
+    no_moon = False
+    try:
+        desert_df = data[desert_dict.keys()]
+    except:
+        print('No desert')
+        no_desert = True
+        desert_df = pd.DataFrame()
 
-    desert_df = desert_df.T
+    try:
+        moon_df = data[moon_dict.keys()]
+    except:
+        print('No moon')
+        no_moon = True
+        moon_df = pd.DataFrame()
+
+    if no_desert and no_moon:
+        print('No valid data to process')
+        return None
+
+    desert_df = desert_df.T 
     moon_df = moon_df.T
+    cols = desert_df.columns if not no_desert else moon_df.columns
 
     processed_data = pd.DataFrame(['ID', 'Desert', 'Moon'])
-    for col in desert_df.columns:
-        ranking_desert = desert_df.sort_values(by=col, ascending=True)[0]
-        ranking_moon = moon_df.sort_values(by=col, ascending=False)
-        str_ranking_desert = ranking_desert.index
-        str_ranking_moon = ranking_moon.index
+    for col in cols:
+        ranking_desert = desert_df.sort_values(by=col, ascending=True)[0] if not no_desert else []
+        ranking_moon = moon_df.sort_values(by=col, ascending=True)[0] if not no_moon else []
 
-        # mapp the ranking with desert and moon dict
-        final_desert_ranking = []
-        final_moon_ranking = []
-        for item in str_ranking_desert:
-            final_desert_ranking.append(desert_dict[item])
-        for item in str_ranking_moon:
-            final_moon_ranking.append(moon_dict[item])
+        # map the ranking with desert and moon dict
+        final_desert_ranking = [desert_dict[item] for item in ranking_desert.index] if not no_desert else []
+        final_moon_ranking = [moon_dict[item] for item in ranking_moon.index] if not no_moon else []
+
         name = task + str(col)
         processed_data[col] = [name, final_desert_ranking, final_moon_ranking]
 
     processed_data = processed_data.T
+    print(processed_data)
     processed_data.columns = ['ID', 'Desert', 'Moon']
     return processed_data
 
 if __name__ == '__main__':
-    data = pd.read_excel('Surival D.xlsx')
+    data = pd.read_excel('Survivial_1.xlsx')
     task = 'D'
     processed_data = process(data, task)
-    processed_data.to_csv('processed_data.csv', index=False)
+    if processed_data is not None:
+        processed_data.to_csv('processed_data.csv', index=False)
